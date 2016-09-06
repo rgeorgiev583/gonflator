a145c5d479c0b443a26bad8ffcecc0ffb61b7e2f
Radoslav Georgiev <rgeorgiev583@gmail.com>
2016-09-06 15:44:31 +0300

    redesign: reworked the buffered tree interface and implementation so that now a capacity can be set for storing settings in its local cache; renamed its methods, added a method for assigning settings only to the local cache and dropped the local cache getter

--- tree/config_buffered.go
+++ tree/config_buffered.go
@@ -1,48 +1,92 @@
 package tree
 
 type BufferedConfigurationServer interface {
-	GetBufferedConfiguration() Configuration
-	PrebufferConfiguration() Configuration
-	PrebufferConfigurationSubtree(prefix string) Configuration
+	SetConfigurationLocal(conf Configuration)
+	FetchConfiguration() error
+	FetchConfigurationSubtree(prefix string) error
 }
 
 type BufferedConfigurationTree struct {
 	ConfigurationTree
 	Configuration
+	Cap int
 }
 
-func loadConfiguration(conf Configuration, bct *BufferedConfigurationTree) Configuration {
-	for path, value := range bct.ConfigurationTree.GetConfiguration() {
-		conf[path] = value
-	}
+type BufferOverflowError struct {
+	Len, Cap int
+}
 
-	return conf
+func (err *BufferOverflowError) Error() string {
+	return fmt.Sprintf("Configuration tree buffer has overflown: its size could not be increased to %d because that is greater than its capacity of %d.", err.Len, err.Cap)
 }
 
 func (bct *BufferedConfigurationTree) GetConfiguration() Configuration {
-	conf := make(Configuration)
+	conf := bct.ConfigurationTree.GetConfiguration()
+	bct.AppendToConfiguration(conf)
+	return conf
+}
+
+func (bct *BufferedConfigurationTree) AppendToConfiguration(conf Configuration) {
+	bct.ConfigurationTree.AppendToConfiguration(conf)
 
 	for path, value := range bct.Configuration {
 		conf[path] = value
 	}
-
-	return loadConfiguration(conf, bct.ConfigurationTree.GetConfiguration())
 }
 
-func (bct *BufferedConfigurationTree) GetBufferedConfiguration() Configuration {
-	return bct.Configuration
+func (bct *BufferedConfigurationTree) SetConfiguration(conf Configuration) (err error) {
+	for path, value := range conf {
+		err = bct.SetSetting(path, value)
+		if err != nil {
+			return
+		}
+	}
+	return
 }
 
-func (bct *BufferedConfigurationTree) PrebufferConfiguration() Configuration {
-	return loadConfiguration(bct.Configuration, bct.ConfigurationTree.GetConfiguration())
-}
-
-func (bct *BufferedConfigurationTree) PrebufferConfigurationSubtree(prefix string) (Configuration, error) {
-	handler, err := bct.SubtreeHandlers[prefix]
-
+func (bct *BufferedConfigurationTree) GetSetting(path string) (value *Setting, err error) {
+	value, err = bct.Configuration[path]
 	if err != nil {
-		return bct.Configuration, &NonexistentSubtreeHandlerError{prefix}
+		value, err = ConfigurationTree.GetSetting(path)
+	}
+	return
+}
+
+func (bct *BufferedConfigurationTree) SetSetting(path string, value *Setting) error {
+	if len(bct.Configuration) == bct.Cap {
+		return bct.ConfigurationTree.SetSetting(path, value)
 	}
 
-	return loadConfiguration(bct.Configuration, handler.GetConfiguration())
+	bct.Configuration[path] = value
+	return
+}
+
+func (bct *BufferedConfigurationTree) SetConfigurationLocal(conf Configuration) (err error) {
+	for path, value := range conf {
+		if len(bct.Configuration) == bct.Cap {
+			return &BufferOverflowError{Len: len(bct.Configuration), Cap: bct.Cap}
+		}
+		bct.Configuration[path] = value
+	}
+	return
+}
+
+func (bct *BufferedConfigurationTree) FetchConfiguration() (err error) {
+	for prefix, handler := range bct.ConfigurationTree.SubtreeHandlers {
+		err = bct.SetConfigurationLocal(handler.GetConfiguration())
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+func (bct *BufferedConfigurationTree) FetchConfigurationSubtree(prefix string) (err error) {
+	handler, err := bct.ConfigurationTree.SubtreeHandlers[prefix]
+	if err != nil {
+		return &NonexistentSubtreeHandlerError{prefix}
+	}
+
+	err = bct.SetConfigurationLocal(handler.GetConfiguration())
+	return
 }
