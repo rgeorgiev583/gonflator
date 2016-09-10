6ba112463f2692ae053a50b6296463a47ca17288
Radoslav Georgiev <rgeorgiev583@gmail.com>
2016-09-11 01:25:03 +0300

    dropped the DialURL function which had no purpose at all

--- remote.go
+++ remote.go
@@ -9,16 +9,3 @@ type Socket struct {
 type Communicator interface {
 	Send(message string, endpoints *Socket) error
 }
-
-func DialURL(url url.URL, proto Protocol) (comm Communicator, err error) {
-	if url.User.Username != "" {
-		user := url.User.Username
-	} else {
-		user := os.Getenv("USER")
-	}
-
-	switch url.Scheme {
-	default:
-		return DialSSH(url.Host, user, "")
-	}
-}
